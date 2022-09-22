65d22ab349b2ad5fb3aad4442be76a58fc5c9bf8
Alonesa <alenakrasnov@mail.ru>
2022-09-22 20:28:26 +0300

    added mapping for our cars list and unique keys

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Cars/>
+      <Cars cars={topCars}/>
       </header>
     </div>
   );
--- src/Cars.tsx
+++ src/Cars.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
+type CarsType = {
+  cars: Array<CarType>
+}
 
-export const Cars = () => {
+type CarType = {
+  manufacturer: string
+  model: string
+}
+
+export const Cars = (props: CarsType) => {
   return (
-	<ul>
-	  <li>car</li>
-	</ul>
-  );
+	<>
+	  <h3>We have available cars for a rent</h3>
+	  <ul style={{listStyle:"none", textAlign:"left"}}>
+		{props.cars.map((car, index) => {
+		  return (
+		    <li key={index}>
+			<span>Manufacturer: {car.manufacturer} </span>
+			<span>Model: {car.model.toUpperCase()} </span>
+		  </li>
+		  )
+		})}
+		{/*{props.cars.map(option => {*/}
+		{/*  <li>*/}
+		{/*	<span>Manufacturer: {option.manufacturer}</span>*/}
+		{/*	<span>Model: {option.model}</span>*/}
+		{/*  </li>*/}
+		{/*})}*/}
+	  </ul>
+	</>
+  )
+	;
 };
