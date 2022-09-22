3fba0a0a18243c02fdf082a6316f7effecab31e3
Alonesa <alenakrasnov@mail.ru>
2022-09-22 20:54:56 +0300

    added 2nd method to our map

--- src/Cars.tsx
+++ src/Cars.tsx
@@ -12,23 +12,41 @@ type CarType = {
 export const Cars = (props: CarsType) => {
   return (
 	<>
-	  <h3>We have available cars for a rent</h3>
-	  <ul style={{listStyle:"none", textAlign:"left"}}>
-		{props.cars.map((car, index) => {
-		  return (
-		    <li key={index}>
-			<span>Manufacturer: {car.manufacturer} </span>
-			<span>Model: {car.model.toUpperCase()} </span>
-		  </li>
-		  )
-		})}
-		{/*{props.cars.map(option => {*/}
-		{/*  <li>*/}
-		{/*	<span>Manufacturer: {option.manufacturer}</span>*/}
-		{/*	<span>Model: {option.model}</span>*/}
-		{/*  </li>*/}
-		{/*})}*/}
-	  </ul>
+	  <div>
+		<h3>We have available cars for a rent in li</h3>
+		<ul style={{listStyle: 'none', textAlign: 'left'}}>
+		  {props.cars.map((car, index) => {
+			return (
+			  <li key={index}>
+				<span>Manufacturer: {car.manufacturer} </span>
+				<span>Model: {car.model.toUpperCase()} </span>
+			  </li>
+			)
+		  })}
+		</ul>
+	  </div>
+	  <div style={{width:'80%'}}>
+		<h3>Available cars in table</h3>
+		<table style={{width:'100%'}}>
+		  <tbody>
+		  <tr key={'01'}>
+			<th>Number</th>
+			<th>Manufacturer</th>
+			<th>Model</th>
+		  </tr>
+		  {props.cars.map((car, index) => {
+			return (
+			  <tr key={index}>
+				<td>{index + 1}</td>
+				<td>{car.manufacturer}</td>
+				<td>{car.model}</td>
+			  </tr>
+			)
+		  })}
+		  </tbody>
+		</table>
+	  
+	  </div>
 	</>
   )
 	;
