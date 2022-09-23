ddc8cb595062194ddaf771ae74e1e3c549634155
Alonesa <alenakrasnov@mail.ru>
2022-09-23 21:21:00 +0300

    moved items from filter to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {MapMethod} from './components/MapMethod';
 import {Button} from './components/Button';
@@ -11,6 +11,36 @@ function App() {
     {manufacturer: 'Mercedes', model: 'e63s'},
     {manufacturer: 'Audi', model: 'rs6'}
   ]
+  
+  const [money, setMoney] = useState([
+    {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+    {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+    {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+    {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+    {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+    {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+    {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+    {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+  ])
+  
+  type filterType = 'All' | 'Dollars' | 'RUBLS';
+  let [filter, setFilter] = useState<filterType>('All')
+  
+  let wallet = money
+  
+  if (filter === 'Dollars') {
+    wallet = money.filter(m => m.banknots === filter)
+  }
+  
+  if (filter === 'RUBLS') {
+    wallet = money.filter(m => m.banknots === filter)
+  }
+  
+  const selectHandler = (option: filterType) => {
+    setFilter(option)
+  }
+  
+  
   return (
     <div className="App">
       <MapMethod cars={topCars}/>
--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,35 +1,6 @@
 import React, {useState} from 'react';
 
 export const Filter = () => {
-  const [money, setMoney] = useState([
-	{banknots: 'Dollars', value: 100, number: ' a1234567890'},
-	{banknots: 'Dollars', value: 50, number: ' z1234567890'},
-	{banknots: 'RUBLS', value: 100, number: ' w1234567890'},
-	{banknots: 'Dollars', value: 100, number: ' e1234567890'},
-	{banknots: 'Dollars', value: 50, number: ' c1234567890'},
-	{banknots: 'RUBLS', value: 100, number: ' r1234567890'},
-	{banknots: 'Dollars', value: 50, number: ' x1234567890'},
-	{banknots: 'RUBLS', value: 50, number: ' v1234567890'},
-  ])
-  
-  type filterType = 'All' | 'Dollars' | 'RUBLS';
-  let [filter, setFilter] = useState<filterType>('All')
-  
-  let wallet = money
-  
-  if (filter === 'Dollars') {
-	wallet = money.filter(m => m.banknots === filter)
-  }
-
-  if (filter === 'RUBLS') {
-	wallet = money.filter(m => m.banknots === filter)
-  }
-  
-  
-  const selectHandler = (option: filterType) => {
-	setFilter(option)
-  }
-  
   
   return (
 	<>
